bb9b2152e8d8c5de1617c878740ab4651906b917
CerberusOne <aingaran2003@gmail.com>
2017-02-09 19:36:05 -0800

    added template for views, menubar, logo. Changed base controller (config), MY_Controller and welcome constructor
    Signed-off-by: CerberusOne <aingaran2003@gmail.com>

--- application/views/template.php
+++ application/views/template.php
@@ -6,10 +6,20 @@
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
+		
 	</head>
 	<body>
         <div id="container">
-			{content}
+			<div class="navbar" role = "navigation">
+                <div class="navbar-inner">
+				
+				{menubar}
+				</div>
+            </div>           
+            <div id="content">
+                <h1>{pagetitle}</h1>
+                {content}
+            </div>
 			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. 
 				{ci_version}</p>
         </div>
