727f950d28f24621185b801ee2e0e60b28037470
Matt Goerwell <superninja246@hotmail.com>
2017-02-10 19:05:21 -0800

    Implementing limited support for parts subpages

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['a1'] = 'roboparts/detail/1';
 $route['parts'] = 'roboparts';
+
